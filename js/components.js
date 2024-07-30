1bb69251e8ff4d24b7657e9e26ba42a9d0084eff
Felix Taylor <felixtaylor898@gmail.com>
2024-07-30 11:15:35 -0400

    Added perform page, made a few changes

--- js/components.js
+++ js/components.js
@@ -6,17 +6,18 @@ class Header extends HTMLElement {
           <div class="mobile rainbow-shadow">
               <nav class="navbar navbar-dark bg-dark">
                   <div class="container-fluid">
-                      <a class="navbar-brand" href="/">Home</a>
+                      <a class="navbar-brand" href="index.html">Home</a>
                       <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse"
                           data-target="#collapsingNavbar3Home">
                           <span class="navbar-toggler-icon"></span>
                       </button>
                       <div class="navbar-collapse collapse" id="collapsingNavbar3Home">
                           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                              <li class="nav-item"><a class="nav-link" href="volunteer.html">Volunteer</a></li>
+                              <li class="nav-item"><a class="nav-link" href="perform.html">Perform</a></li>
+                              <li class="nav-item"><a class="nav-link" href="donate.html">Donate</a></li>
                               <li class="nav-item"><a class="nav-link" href="vendors.html">Vendors</a></li>
                               <li class="nav-item"><a class="nav-link" href="sponsor.html">Sponsor</a></li>
-                              <li class="nav-item"><a class="nav-link" href="donate.html">Donate</a></li>
-                              <li class="nav-item"><a class="nav-link" href="volunteer.html">Volunteer</a></li>
                               <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
                               <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
                           </ul>
@@ -41,13 +42,14 @@ class Header extends HTMLElement {
               <div class="rainbow-shadow">
                   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                       <div class="container-fluid">
-                          <a class="navbar-brand" href="/">Home</a>
+                          <a class="navbar-brand" href="index.html">Home</a>
                           <div class="collapse navbar-collapse">
                               <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav-fill w-100">
+                                  <li class="nav-item"><a class="nav-link" href="volunteer.html">Volunteer</a></li>
+                                  <li class="nav-item"><a class="nav-link" href="perform.html">Perform</a></li>
+                                  <li class="nav-item"><a class="nav-link" href="donate.html">Donate</a></li>
                                   <li class="nav-item"><a class="nav-link" href="vendors.html">Vendors</a></li>
                                   <li class="nav-item"><a class="nav-link" href="sponsor.html">Sponsor</a></li>
-                                  <li class="nav-item"><a class="nav-link" href="donate.html">Donate</a></li>
-                                  <li class="nav-item"><a class="nav-link" href="volunteer.html">Volunteer</a></li>
                                   <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
                                   <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
 
